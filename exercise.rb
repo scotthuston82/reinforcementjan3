25a37059d2efc895e210a05d4889c47eceee0985
Scott <scotthuston1982@gmail.com>
2018-01-03 13:02:17 -0500

    final commit

---
+++ exercise.rb
@@ -0,0 +1,14 @@
+data = { data: { rooms:
+    [ { id: 1, room_number: "201", capacity: 50}, { id: 2, room_number: "301", capacity: 200 }, { id: 3, room_number: "1B", capacity: 100}
+    ],
+    events: [
+      { id: 1, room_id: 2, start_time: 18, end_time: 20, attendees: 75 },
+      { id: 2, room_id: 1, start_time: 10, end_time: 18, attendees: 25 },
+      { id: 3, room_id: 2, start_time: 10, end_time: 18, attendees: 20 },
+      { id: 4, room_id: 3, start_time: 18, end_time: 21, attendees: 56 },
+    ]
+  }
+}
+
+room = data[:data][:rooms][0][:capacity]
+p room
